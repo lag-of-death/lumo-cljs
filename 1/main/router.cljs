c231b7d8cce7fb118d94deb0aaa2d56385990178
maggot <mateuszwit21@gmail.com>
2017-05-08 08:37:56 +0200

    Add basic auth example

--- 1/main/router.cljs
+++ 1/main/router.cljs
@@ -2,7 +2,12 @@
 
 (defonce express (js/require "express"))
 
+(defonce basic-auth (js/require "express-basic-auth"))
+
 (defonce router
   (-> (.Router express)
-      (.get "/show-cookies" (fn [req res] (.send res req.cookies)))
-      (.get "/some" (fn [req res] (.send res "OK")))))
+      (.get "/show-cookies"
+            (fn [req res] (.send res req.cookies)))
+      (.get "/some"
+            (basic-auth (clj->js {:users {:admin :admin} :challenge true}))
+            (fn [req res] (.send res "OK")))))
